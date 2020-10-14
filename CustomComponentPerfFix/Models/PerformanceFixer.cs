9d8850fa19aba20d495ad4df77dea3898b3ef66c
Mhburg <Morgan_lee5@hotmail.com>
2020-10-14 16:24:56 -0400

    Add classes to support dynamic loading

---
+++ CustomComponentPerfFix/Models/PerformanceFixer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueTechPerfFixes
+{
+    /// <summary>
+    /// This class encapsulates what is needed to improve performance for an dll.
+    /// </summary>
+    public class PerformanceFixer
+    {
+        public bool Enable { get; private set; }
+    }
+}
